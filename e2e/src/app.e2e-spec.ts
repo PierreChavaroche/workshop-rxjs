525f38b3338bc0bda05423bf08262298d55f9fed
Pierre Chavaroche <info@chavaroche.de>
2020-08-01 13:34:15 +0200

    Renaming

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('rxjs app is running!');
+    expect(page.getTitleText()).toEqual('workshop-rxjs app is running!');
   });
 
   afterEach(async () => {
